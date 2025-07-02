5532dc43fd2f78674ee27df54e1730b314a7947f
abu-jafor <abujaforwd@yahoo.com>
2025-07-02 08:13:10 +0600

    if els statment

---
+++ day-3/class-1.py
@@ -0,0 +1,12 @@
+
+
+
+print("Welcome to the roalercoster!")
+age = int(input("What is your age? \n"))
+
+if age >18:
+     print("You can drive a car")
+elif age == 18:
+     print("You can drive a car")
+else:
+     print("You can't drive a car")
